f78b5bfce80450b62a03deb28146a0c323b247ff
suryanarayanandv <dbvenkat857@gmail.com>
2024-02-04 07:34:41 +0530

    Added Application Constants

---
+++ src/main/java/com/github/passdrive/utils/constants/UsbConstants.java
@@ -0,0 +1,11 @@
+package com.github.passdrive.utils.constants;
+
+public class UsbConstants {
+    // GLOBAL CONSTANTS
+    public static final String OS_NAME = System.getProperty("os.name");
+    public static final long DETECTION_DELAY_MS = 5000;
+
+    // WINDOWS CONSTANTS
+    public static final int WIN_DEVICE_TPE = 2;
+    public static final String WMIC_PATH = "C:\\Windows\\System32\\Wbem\\WMIC.exe";
+}
