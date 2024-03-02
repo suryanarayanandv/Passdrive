67c07b4b29aaa5a6765263b0dc34dd8f9faae955
suryanarayanandv <dbvenkat857@gmail.com>
2024-03-03 00:29:01 +0530

    App Consts

---
+++ src/main/java/com/github/passdrive/utils/constants/AppConstants.java
@@ -0,0 +1,12 @@
+package com.github.passdrive.utils.constants;
+
+public class AppConstants {
+    public static int SUCCESS = 200;
+    public static int FAILURE = 400;
+    public static int NOT_FOUND = 404;
+    public static int CREATED = 201;
+    public static int UNAUTHORIZED = 401;
+    
+    public static int LOGGEDIN = 204;
+    public static int LOGGEDOUT = 403;
+}
