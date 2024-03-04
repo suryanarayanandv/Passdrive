f3a1f83184027c886d2b93e92dd095d6197c3ba7
suryanarayanandv <dbvenkat857@gmail.com>
2024-03-04 07:58:05 +0530

    Result: data

--- src/main/java/com/github/passdrive/utils/Result.java
+++ src/main/java/com/github/passdrive/utils/Result.java
@@ -6,16 +6,23 @@ import com.google.gson.JsonObject;
 public class Result {
     private int code;
     private String message;
+    private JsonObject data = null;
 
     public Result(int code, String message) {
         this.code = code;
         this.message = message;
     }
 
+    public Result(int code, String message, JsonObject data) {
+        this(code, message);
+        this.data = data;
+    }
+
     public JsonElement toJson() {
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("code", code);
+        jsonObject.addProperty("status", code);
         jsonObject.addProperty("message", message);
+        jsonObject.addProperty("data", data.getAsString());
         return jsonObject;
     }
 }
