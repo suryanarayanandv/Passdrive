575b8721b81266635841c8e407401dc606b2b3fa
suryanarayanandv <dbvenkat857@gmail.com>
2024-03-11 22:30:18 +0530

    Redundant: app level loop, Initial Configs

--- src/main/java/com/github/passdrive/Main.java
+++ src/main/java/com/github/passdrive/Main.java
@@ -9,7 +9,9 @@ import com.github.passdrive.pendrive.PasswordManager;
 import com.github.passdrive.protector.configProtection.configurePendrive;
 import com.github.passdrive.protector.configProtection.detectConfiguration;
 import com.github.passdrive.usbDetector.UsbDevice;
+import com.github.passdrive.usbDetector.manager.DetectTaskSchduler;
 import com.github.passdrive.utils.Result;
+import com.github.passdrive.utils.configPopulator;
 import com.github.passdrive.utils.constants.AppConstants;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -21,155 +23,166 @@ public class Main {
     // Chrome extention native app host
     // entry point
     public static void main(String[] args) throws IOException {
-        while (true) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            Boolean isDetected = (Boolean) EnvironmentImpl.getEnvironmentMap("detected");
-            Boolean isLogged = (Boolean) EnvironmentImpl.getEnvironmentMap("logged");
+        if ( !configPopulator.isPopulated ) {
+            configPopulator.populate();
+        }
 
-            if (isDetected && !isLogged) {
-                UsbDevice usb = (UsbDevice) EnvironmentImpl.getEnvironmentMap("usbdevice");
-                // Read message from Chrome
-                String input = reader.readLine();
+        if ( EnvironmentImpl.getEnvironmentMap("detected") == null ) {
+            EnvironmentImpl.setEnvironmentMap("detected", Boolean.FALSE);
 
-                // Parse JSON message
-                JsonElement jsonElement = JsonParser.parseString(input);
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
+            // Start Detector
+            DetectTaskSchduler detectTaskSchduler = new DetectTaskSchduler();
+            detectTaskSchduler.start();
+        }
 
-                // data: {"type": "", "data": }}
-                String requestType = jsonObject.get("type").getAsString();
+        // TODO: check if the while loop is necessary
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Boolean isDetected = (Boolean) EnvironmentImpl.getEnvironmentMap("detected");
+        Boolean isLogged = (Boolean) EnvironmentImpl.getEnvironmentMap("logged");
 
-                if ("POST".equals(requestType.split("_")[0])) {
-                    if (requestType.contains("login")) {
-                        // Login
-                        JsonObject data = jsonObject.get("data").getAsJsonObject();
-                        String masterPassword = data.get("master").getAsString();
+        if (isDetected && !isLogged) {
+            UsbDevice usb = (UsbDevice) EnvironmentImpl.getEnvironmentMap("usbdevice");
+            // Read message from Chrome
+            String input = reader.readLine();
 
-                        // Check Configuration
-                        if (detectConfiguration.detect(usb)) {
+            // Parse JSON message
+            JsonElement jsonElement = JsonParser.parseString(input);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            // data: {"type": "", "data": }}
+            String requestType = jsonObject.get("type").getAsString();
+
+            if ("POST".equals(requestType.split("_")[0])) {
+                if (requestType.contains("login")) {
+                    // Login
+                    JsonObject data = jsonObject.get("data").getAsJsonObject();
+                    String masterPassword = data.get("master").getAsString();
+
+                    // Check Configuration
+                    if (detectConfiguration.detect(usb)) {
+                        // Check Master Password
+                        if (PasswordManager.checkMasterPassword(usb, masterPassword)) {
+                            EnvironmentImpl.setEnvironmentMap("logged", Boolean.TRUE);
+                            isLogged = true;
+                            System.out.println(new Result(AppConstants.LOGGEDIN, "Logged in successfully").toJson()
+                                    .getAsString());
+                        } else {
+                            System.out.println(
+                                    new Result(AppConstants.UNAUTHORIZED, "Unauthorized").toJson().getAsString());
+                        }
+                    } else {
+                        if (configurePendrive.configure(usb, masterPassword)) {
                             // Check Master Password
                             if (PasswordManager.checkMasterPassword(usb, masterPassword)) {
                                 EnvironmentImpl.setEnvironmentMap("logged", Boolean.TRUE);
                                 isLogged = true;
-                                System.out.println(new Result(AppConstants.LOGGEDIN, "Logged in successfully").toJson()
-                                        .getAsString());
+                                System.out.println(new Result(AppConstants.LOGGEDIN, "Logged in successfully")
+                                        .toJson().getAsString());
                             } else {
                                 System.out.println(
                                         new Result(AppConstants.UNAUTHORIZED, "Unauthorized").toJson().getAsString());
                             }
                         } else {
-                            if (configurePendrive.configure(usb, masterPassword)) {
-                                // Check Master Password
-                                if (PasswordManager.checkMasterPassword(usb, masterPassword)) {
-                                    EnvironmentImpl.setEnvironmentMap("logged", Boolean.TRUE);
-                                    isLogged = true;
-                                    System.out.println(new Result(AppConstants.LOGGEDIN, "Logged in successfully")
-                                            .toJson().getAsString());
-                                } else {
-                                    System.out.println(
-                                            new Result(AppConstants.UNAUTHORIZED, "Unauthorized").toJson().getAsString());
-                                }
-                            } else {
-                                System.out.println(
-                                        new Result(AppConstants.FAILURE, "Configuration failed").toJson().getAsString());
-                            }
+                            System.out.println(
+                                    new Result(AppConstants.FAILURE, "Configuration failed").toJson().getAsString());
                         }
                     }
                 }
             }
+        }
 
-            if (isDetected && isLogged && !((Boolean) EnvironmentImpl.getEnvironmentMap("removed"))) {
-                UsbDevice usb = (UsbDevice) EnvironmentImpl.getEnvironmentMap("usbdevice");
-                if (usb.isRemoved()) {
-                    // TODO: Send message to Thread1 to start detection again
+        if (isDetected && isLogged && !((Boolean) EnvironmentImpl.getEnvironmentMap("removed"))) {
+            UsbDevice usb = (UsbDevice) EnvironmentImpl.getEnvironmentMap("usbdevice");
+            if (usb.isRemoved()) {
+                // TODO: Send message to Thread1 to start detection again
 
-                    EnvironmentImpl.setEnvironmentMap("removed", Boolean.TRUE);
-                    System.out
-                            .println(new Result(AppConstants.LOGGEDOUT, "Logged out successfully").toJson().getAsString());
-                }
+                EnvironmentImpl.setEnvironmentMap("removed", Boolean.TRUE);
+                System.out
+                        .println(new Result(AppConstants.LOGGEDOUT, "Logged out successfully").toJson().getAsString());
+            }
 
-                // Read message from Chrome
-                String input = reader.readLine();
+            // Read message from Chrome
+            String input = reader.readLine();
 
-                // Parse JSON message
-                JsonElement jsonElement = JsonParser.parseString(input);
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
+            // Parse JSON message
+            JsonElement jsonElement = JsonParser.parseString(input);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
 
-                // data: {"type": "", "data": }}
-                String requestType = jsonObject.get("type").getAsString();
+            // data: {"type": "", "data": }}
+            String requestType = jsonObject.get("type").getAsString();
 
-                if ("POST".equals(requestType.split("_")[0])) {
-                    if (requestType.contains("domain")) {
-                        // Set domain password
-                        JsonObject data = jsonObject.get("data").getAsJsonObject();
-                        String domainName = data.get("domain").getAsString();
-                        String subDomainName = data.get("subdomain").getAsString();
-                        String username = data.get("username").getAsString();
-                        String domainPassword = data.get("password").getAsString();
+            if ("POST".equals(requestType.split("_")[0])) {
+                if (requestType.contains("domain")) {
+                    // Set domain password
+                    JsonObject data = jsonObject.get("data").getAsJsonObject();
+                    String domainName = data.get("domain").getAsString();
+                    String subDomainName = data.get("subdomain").getAsString();
+                    String username = data.get("username").getAsString();
+                    String domainPassword = data.get("password").getAsString();
 
-                        if (PasswordManager.storePassword(usb, domainName, subDomainName, username, domainPassword)) {
-                            System.out.println(new Result(AppConstants.SUCCESS, "Domain password set successfully")
-                                    .toJson().getAsString());
-                        } else {
-                            System.out.println(
-                                    new Result(AppConstants.FAILURE, "Domain password set failed").toJson().getAsString());
-                        }
+                    if (PasswordManager.storePassword(usb, domainName, subDomainName, username, domainPassword)) {
+                        System.out.println(new Result(AppConstants.SUCCESS, "Domain password set successfully")
+                                .toJson().getAsString());
+                    } else {
+                        System.out.println(
+                                new Result(AppConstants.FAILURE, "Domain password set failed").toJson().getAsString());
                     }
                 }
+            }
 
-                if ("GET".equals(requestType.split("_")[0])) {
-                    if (requestType.contains("domain")) {
-                        // Get domain password
-                        JsonObject data = jsonObject.get("data").getAsJsonObject();
-                        String domainName = data.get("domain").getAsString();
-                        String subDomainName = data.get("subdomain").getAsString();
+            if ("GET".equals(requestType.split("_")[0])) {
+                if (requestType.contains("domain")) {
+                    // Get domain password
+                    JsonObject data = jsonObject.get("data").getAsJsonObject();
+                    String domainName = data.get("domain").getAsString();
+                    String subDomainName = data.get("subdomain").getAsString();
 
-                        JsonObject domainPassword = PasswordManager.getPassword(usb, domainName, subDomainName);
-                        if (domainPassword != null) {
-                            JsonObject response = new JsonObject();
-                            response.add("data", domainPassword);
-                            System.out.println(
-                                    new Result(AppConstants.SUCCESS, "success", response).toJson().getAsString());
-                        } else {
-                            System.out.println(
-                                    new Result(AppConstants.NOT_FOUND, "Domain password not found").toJson().getAsString());
-                        }
+                    JsonObject domainPassword = PasswordManager.getPassword(usb, domainName, subDomainName);
+                    if (domainPassword != null) {
+                        JsonObject response = new JsonObject();
+                        response.add("data", domainPassword);
+                        System.out.println(
+                                new Result(AppConstants.SUCCESS, "success", response).toJson().getAsString());
+                    } else {
+                        System.out.println(
+                                new Result(AppConstants.NOT_FOUND, "Domain password not found").toJson().getAsString());
+                    }
 
+                }
+            }
+
+            if ("DELETE".equals(requestType.split("_")[0])) {
+                if (requestType.contains("domain")) {
+                    // Delete domain password
+                    JsonObject data = jsonObject.get("data").getAsJsonObject();
+                    String domainName = data.get("domain").getAsString();
+                    String subDomainName = data.get("subdomain").getAsString();
+
+                    if (PasswordManager.removePassword(usb, domainName, subDomainName)) {
+                        System.out.println(new Result(AppConstants.SUCCESS, "Domain password deleted successfully")
+                                .toJson().getAsString());
+                    } else {
+                        System.out.println(new Result(AppConstants.FAILURE, "Domain password delete failed")
+                                .toJson().getAsString());
                     }
                 }
+            }
 
-                if ("DELETE".equals(requestType.split("_")[0])) {
-                    if (requestType.contains("domain")) {
-                        // Delete domain password
-                        JsonObject data = jsonObject.get("data").getAsJsonObject();
-                        String domainName = data.get("domain").getAsString();
-                        String subDomainName = data.get("subdomain").getAsString();
+            if ("PUT".equals(requestType.split("_")[0])) {
+                if (requestType.contains("domain")) {
+                    // Update domain password
+                    JsonObject data = jsonObject.get("data").getAsJsonObject();
+                    String domainName = data.get("domain").getAsString();
+                    String subDomainName = data.get("subdomain").getAsString();
+                    String username = data.get("username").getAsString();
+                    String domainPassword = data.get("password").getAsString();
 
-                        if (PasswordManager.removePassword(usb, domainName, subDomainName)) {
-                            System.out.println(new Result(AppConstants.SUCCESS, "Domain password deleted successfully")
-                                    .toJson().getAsString());
-                        } else {
-                            System.out.println(new Result(AppConstants.FAILURE, "Domain password delete failed")
-                                    .toJson().getAsString());
-                        }
-                    }
-                }
-
-                if ("PUT".equals(requestType.split("_")[0])) {
-                    if (requestType.contains("domain")) {
-                        // Update domain password
-                        JsonObject data = jsonObject.get("data").getAsJsonObject();
-                        String domainName = data.get("domain").getAsString();
-                        String subDomainName = data.get("subdomain").getAsString();
-                        String username = data.get("username").getAsString();
-                        String domainPassword = data.get("password").getAsString();
-
-                        if (PasswordManager.updatePassword(usb, domainName, subDomainName, username, domainPassword)) {
-                            System.out.println(new Result(AppConstants.SUCCESS, "Domain password updated successfully")
-                                    .toJson().getAsString());
-                        } else {
-                            System.out.println(new Result(AppConstants.FAILURE, "Domain password update failed")
-                                    .toJson().getAsString());
-                        }
+                    if (PasswordManager.updatePassword(usb, domainName, subDomainName, username, domainPassword)) {
+                        System.out.println(new Result(AppConstants.SUCCESS, "Domain password updated successfully")
+                                .toJson().getAsString());
+                    } else {
+                        System.out.println(new Result(AppConstants.FAILURE, "Domain password update failed")
+                                .toJson().getAsString());
                     }
                 }
             }
