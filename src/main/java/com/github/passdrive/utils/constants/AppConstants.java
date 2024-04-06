6636a56a1909ad9312a3bdaa70bf606b011622f2
suryanarayanandv <dbvenkat857@gmail.com>
2024-04-06 23:02:27 +0530

    Consts Updates

--- src/main/java/com/github/passdrive/utils/constants/AppConstants.java
+++ src/main/java/com/github/passdrive/utils/constants/AppConstants.java
@@ -1,6 +1,9 @@
 package com.github.passdrive.utils.constants;
 
 public class AppConstants {
+    public static int WAIT_THREASHOLD_ROUNDS = 20;
+    public static int JOB_PENDING = 99;
+
     public static int SUCCESS = 200;
     public static int FAILURE = 400;
     public static int NOT_FOUND = 404;
@@ -9,4 +12,9 @@ public class AppConstants {
     
     public static int LOGGEDIN = 204;
     public static int LOGGEDOUT = 403;
+
+    public static int READ = 0;
+    public static int WRITE = 1;
+    public static int UPDATE = 2;
+    public static int DELETE = 3;
 }
